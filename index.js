e9736dae7cc04c1ef8955743be8db875c1d5a815
huntermahmud <mahmudthedatahunter@gmail.com>
2024-05-11 08:48:30 +0600

    borrow book info saved to db with preventing user to take single book multiple time

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 3000;
@@ -8,15 +9,18 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 //middleware
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    //other links will be here
-  ],
-  // credentials: true,
-}))
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      //other links will be here
+    ],
+    // credentials: true,
+  })
+);
 
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USE}:${process.env.DB_PASS}@cluster0.9wkdqn0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -31,37 +35,37 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-   
     const booksCollection = client.db("bookDB").collection("books");
     const categoriesCollection = client.db("bookDB").collection("categories");
+    const borrowCollection = client.db("bookDB").collection("borrows");
 
     // books related api
     //load all books data
-    app.get("/allBooks", async(req, res) => {
+    app.get("/allBooks", async (req, res) => {
       const result = await booksCollection.find().toArray();
       res.send(result);
     });
-    
+
     // add new book info db
-    app.post("/addBook", async(req, res)=> {
+    app.post("/addBook", async (req, res) => {
       const bookInfo = req.body;
-      console.log(bookInfo);
+      // console.log(bookInfo);
       const result = await booksCollection.insertOne(bookInfo);
       res.send(result);
-    })
+    });
 
     /// update/details page books data of single book
-    app.get("/books/:id", async(req, res)=> {
+    app.get("/books/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
-      const result = await booksCollection.findOne(query)
+      const query = { _id: new ObjectId(id) };
+      const result = await booksCollection.findOne(query);
       res.send(result);
-    })
+    });
     //update book by id
-    app.patch('/updatebook/:id', async(req, res)=>{
+    app.patch("/updatebook/:id", async (req, res) => {
       const info = req.body;
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const updateInfo = {
         $set: {
           ...info,
@@ -69,37 +73,74 @@ async function run() {
       };
       const result = await booksCollection.updateOne(query, updateInfo);
       res.send(result);
-    })
-
+    });
 
     //category related apis
 
-    // get all category 
-    app.get('/categories', async(req, res)=> {
+    // get all category
+    app.get("/categories", async (req, res) => {
       const result = await categoriesCollection.find().toArray();
       res.send(result);
-    })
-    
+    });
 
     //get books by category name
-    app.get('/category/:categoryName', async(req, res)=> {
+    app.get("/category/:categoryName", async (req, res) => {
       const cName = req.params.categoryName;
-      const query = {category: cName};
-      const result = await booksCollection.find(query).toArray()
+      const query = { category: cName };
+      const result = await booksCollection.find(query).toArray();
       // console.log(result);
-      res.send(result)
-    })
+      res.send(result);
+    });
+
+    // borrowed books related api
+    // borrowed books by user email saved to db
+    app.post("/borrow/:id", async (req, res) => {
+      const info = req.body;
+      const id = req.params.id;
+      console.log(info, id);
+      
+      const query = {id: id,borrowerEmail:info.borrowerEmail };
+      console.log(query);
+      const isExist = await borrowCollection.findOne(query);
+      if(isExist){
+        return res.send({exist: true})
+      }
+      const borrowInfo = {
+        ...info,
+      };
+      // console.log(borrowInfo);
+      const result = await borrowCollection.insertOne(borrowInfo);
+      res.send(result);
+    });
+
+    //borrow to decrease the quantity by 1
+
+    app.patch("/books/:id", async (req, res) => {
+      const id = req.params.id;
+      const category = req.body;
+      const query = { _id: new ObjectId(id) };
+      // console.log(category);
+      const updateInfo = {
+        $set: category,
+      };
+      const result = await booksCollection.updateOne(query,updateInfo)
+      res.send(result);
+    });
 
     //jwt related api
 
-    app.post('/jwt', async(req, res)=> {
+    app.post("/jwt", async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.SECRET,{
+      const token = jwt.sign(user, process.env.SECRET, {
         expiresIn: "1h",
-      })
-      res.send(token);
-
-    })
+      });
+      const cookieOptions = {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      };
+      res.cookie("token", token, cookieOptions).send({ status: "success" });
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
