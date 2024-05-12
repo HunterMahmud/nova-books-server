34a955c2a2c27479764ad475055fe29e31824d77
huntermahmud <mahmudthedatahunter@gmail.com>
2024-05-12 18:11:54 +0600

    release for production cors url setup done

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
+      // "http://localhost:5173",
       "https://nova-books.web.app",
       "https://nova-books.firebaseapp.com",
       //other links will be here
